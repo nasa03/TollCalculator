3c5dd9ec67b558e76ff58adaba70d2f7a84e378e
Khosro Jadidi <khosro.jadidi.developer@gmail.com>
2021-08-19 15:59:26 +0200

    wip-add repository
    add controller

---
+++ TollCalculator.API/Controllers/TestingController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using TollCalculator.API.Interfaces;
+using TollCalculator.API.Models;
+using TollCalculator.API.Repositories;
+
+namespace TollCalculator.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class TestingController : Controller
+    {
+        private readonly SQLiteRepository _repository;
+
+        private readonly List<TollEntry> fakeEntries = new List<TollEntry>
+        {
+
+        };
+        public TestingController(SQLiteRepository repository) =>
+            _repository = repository;
+
+    }
+}
