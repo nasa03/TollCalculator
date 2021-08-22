a5583c32bc204f4644481abb21d00c3fca18434d
Khosro Jadidi <khosro.jadidi.developer@gmail.com>
2021-08-22 16:17:44 +0200

    move helper methods to a seperate file and class

---
+++ TollCalculator.API/Controllers/Helpers/TesterHelpers.cs
@@ -0,0 +1,70 @@
+using TollCalculator.API.Builders;
+using TollCalculator.API.Models;
+namespace TollCalculator.API.Controllers.Helpers
+{
+    internal static class TesterHelpers
+    {
+        internal static void AddFakeEntry(List<TollEntry> _fakeEntries)
+        {
+            var entry = BuildFakeEntry();
+            _fakeEntries.Add(entry);
+        }
+        private static TollEntry BuildFakeEntry()
+        {
+            var date = GetRandomDate();
+            var plate = GetRandomLicensePlate();
+            var fee = GetRandomFee();
+            var entryBuilder = new TollEntryBuilder();
+            entryBuilder.WithLicencePlate(plate);
+            entryBuilder.EnteredByDate(date);
+            entryBuilder.ForFeeOf(fee);
+            return entryBuilder.Build();
+        }
+        private static int GetRandomFee()
+        {
+            var rnd = new Random();
+            var probability = rnd.Next(0, 101);
+            if (probability < 50)
+                return 0;
+            var fee = rnd.Next(0, 61);
+            return fee;
+        }
+        private static string GetRandomLicensePlate()
+        {
+            var allLettersAndnumbers = "abcdefghijklmnopqrstuvwxyzåäö0123456789";
+            var rnd = new Random();
+            var licensePlate = new char[6];
+            for (var i = 0; i < licensePlate.Length; i++)
+            {
+                var randomPosition = rnd.Next(0, allLettersAndnumbers.Length - 1);
+                var randomChar = allLettersAndnumbers[randomPosition];
+                licensePlate[i] = randomChar;
+            }
+            return new String(licensePlate);
+        }
+        private static DateTime GetRandomDate()
+        {
+            var year = GetRandomInteger(2015, 2022);
+            var month = GetRandomInteger(1, 13);
+            var day = GetRandomInteger(1, 29);
+            var hour = GetRandomInteger(0, 24);
+            var minute = GetRandomInteger(0, 60);
+            var second = GetRandomInteger(0, 60);
+            var date = new DateTime
+            (
+                year: year,
+                month: month,
+                day: day,
+                hour: hour,
+                minute: minute,
+                second: second
+            );
+            return date;
+        }
+        private static int GetRandomInteger(int a, int b)
+        {
+            var rnd = new Random();
+            return rnd.Next(a, b);
+        }
+    }
+}
--- TollCalculator.API/Controllers/TesterController.cs
+++ TollCalculator.API/Controllers/TesterController.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using TollCalculator.API.Builders;
+using TollCalculator.API.Controllers.Helpers;
 using TollCalculator.API.Interfaces;
 using TollCalculator.API.Models;
-using TollCalculator.API.Repositories;
 namespace TollCalculator.API.Controllers
 {
     [ApiController]
@@ -11,93 +10,22 @@ namespace TollCalculator.API.Controllers
     {
         private readonly IRepository _repository;
         private readonly List<TollEntry> _fakeEntries = new();
-
         public TesterController(IRepository repository)
         {
             _repository = repository;
         }
-
         [HttpPost]
         public IActionResult Reset()
         {
             var resetResult = _repository.DeleteAllEntries();
             if (!resetResult)
                 return NotFound();
-
             for (var i = 0; i < 10000; i++)
-                AddFakeEntry();
-
+                TesterHelpers.AddFakeEntry(_fakeEntries);
             var postResult = _repository.PostTollEntry(_fakeEntries);
-
             if (!postResult)
                 return NotFound();
             return Ok();
         }
-
-        private void AddFakeEntry()
-        {
-            var date = GetRandomDate();
-            var plate = GetRandomLicensePlate();
-            var fee = GetRandomFee();
-
-            var entryBuilder = new TollEntryBuilder();
-            entryBuilder.WithLicencePlate(plate);
-            entryBuilder.EnteredByDate(date);
-            entryBuilder.ForFeeOf(fee);
-
-            var entry = entryBuilder.Build();
-            _fakeEntries.Add(entry);
-        }
-
-        private int GetRandomFee()
-        {
-            var rnd = new Random();
-            var probability = rnd.Next(0, 101);
-            if (probability < 50)
-                return 0;
-            var fee = rnd.Next(0, 61);
-            return fee;
-        }
-
-        private string GetRandomLicensePlate()
-        {
-            var allLettersAndnumbers = "abcdefghijklmnopqrstuvwxyzåäö0123456789";
-            var rnd = new Random();
-            var licensePlate = new char[6];
-            for (var i = 0; i < licensePlate.Length; i++)
-            {
-                var randomPosition = rnd.Next(0, allLettersAndnumbers.Length - 1);
-                var randomChar = allLettersAndnumbers[randomPosition];
-                licensePlate[i] = randomChar;
-            }
-            return new String(licensePlate);
-        }
-
-        private DateTime GetRandomDate()
-        {
-            var year = GetRandomInteger(2015, 2022);
-            var month = GetRandomInteger(1, 13);
-            var day = GetRandomInteger(1, 29);
-            var hour = GetRandomInteger(0, 24);
-            var minute = GetRandomInteger(0, 60);
-            var second = GetRandomInteger(0, 60);
-            var date = new DateTime
-            (
-                year: year,
-                month: month,
-                day: day,
-                hour: hour,
-                minute: minute,
-                second: second
-            );
-            return date;
-        }
-
-        private int GetRandomInteger(int a, int b)
-        {
-            var rnd = new Random();
-            return rnd.Next(a, b);
-        }
-
     }
 }
