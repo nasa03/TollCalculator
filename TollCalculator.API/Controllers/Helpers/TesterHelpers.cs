b790235d841e22ad0e966c441d8082615c1dd3a5
Khosro Jadidi <khosro.jadidi.developer@gmail.com>
2021-08-23 23:05:16 +0200

    cleanup

--- TollCalculator.API/Controllers/Helpers/TesterHelpers.cs
+++ TollCalculator.API/Controllers/Helpers/TesterHelpers.cs
@@ -4,17 +4,6 @@ namespace TollCalculator.API.Controllers.Helpers
 {
     internal static class TesterHelpers
     {
-        // internal static TollEntry GetFakeEntry()
-        // {
-        //     var date = GetRandomDate();
-        //     var plate = GetRandomLicensePlate();
-        //     var fee = GetRandomFee();
-        //     var entryBuilder = new TollEntryBuilder();
-        //     entryBuilder.WithLicencePlate(plate);
-        //     entryBuilder.EnteredByDate(date);
-        //     entryBuilder.ForFeeOf(fee);
-        //     return entryBuilder.Build();
-        // }
         internal static int GetRandomFee()
         {
             var rnd = new Random();
@@ -24,19 +13,7 @@ namespace TollCalculator.API.Controllers.Helpers
             var fee = rnd.Next(0, 61);
             return fee;
         }
-        // private static string GetRandomLicensePlate()
-        // {
-        //     var allLettersAndnumbers = "abcdefghijklmnopqrstuvwxyzåäö0123456789";
-        //     var rnd = new Random();
-        //     var licensePlate = new char[6];
-        //     for (var i = 0; i < licensePlate.Length; i++)
-        //     {
-        //         var randomPosition = rnd.Next(0, allLettersAndnumbers.Length - 1);
-        //         var randomChar = allLettersAndnumbers[randomPosition];
-        //         licensePlate[i] = randomChar;
-        //     }
-        //     return new String(licensePlate);
-        // }
+
         internal static DateTime GetRandomDate()
         {
             var year = GetRandomInteger(2021, 2022);
